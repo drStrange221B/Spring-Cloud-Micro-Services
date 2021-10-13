bb9a12e0f316f59e2d0577a6931e60dee0f26b31
Rojaya Maharjan <rojaya.mh3@gmail.com>
2021-10-13 14:10:17 -0400

    to set the custom classpath

--- src/main/java/com/emrys/springcloud/SpringcloudApplication.java
+++ src/main/java/com/emrys/springcloud/SpringcloudApplication.java
@@ -10,6 +10,9 @@ public class SpringcloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudApplication.class, args);
+
+		System.out.println("Hello fro Spring boot!");
+		System.out.println(System.getProperty("user.dir"));
 	}
 
 }
