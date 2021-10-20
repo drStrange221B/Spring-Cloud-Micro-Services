fe1b1b344c3101c2f9ebbf0add5d67be72c30cef
drStrange221B <50057970+drStrange221B@users.noreply.github.com>
2021-10-20 15:48:26 -0400

    spring-cloud-client commit

--- src/main/java/com/emrys/springcloud/SpringcloudApplication.java
+++ src/main/java/com/emrys/springcloud/SpringcloudApplication.java
@@ -2,17 +2,13 @@ package com.emrys.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+
 
 @SpringBootApplication
-@EnableConfigServer
 public class SpringcloudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudApplication.class, args);
-
-		System.out.println("Hello fro Spring boot!");
-		System.out.println(System.getProperty("user.dir"));
 	}
 
 }
--- src/test/java/com/emrys/springcloud/SpringcloudApplicationTests.java
+++ src/test/java/com/emrys/springcloud/SpringcloudApplicationTests.java
@@ -3,7 +3,12 @@ package com.emrys.springcloud;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(
+		webEnvironment = RANDOM_PORT,
+		properties = {"spring.cloud.config.enabled=false"}
+)
 class SpringcloudApplicationTests {
 
 	@Test
