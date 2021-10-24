016a777dddd493b541543771029b0d8b188ba2e8
drStrange221B <50057970+drStrange221B@users.noreply.github.com>
2021-10-24 13:57:58 -0400

    adding encrypting key for symmetric encryption

--- src/test/java/com/emrys/springcloud/SpringcloudApplicationTests.java
+++ src/test/java/com/emrys/springcloud/SpringcloudApplicationTests.java
@@ -1,13 +1,14 @@
 package com.emrys.springcloud;
 
-import org.junit.jupiter.api.Test;
+
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringcloudApplicationTests {
+public class SpringcloudApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
